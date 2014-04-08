d051bc4eda861d3d2a42044370cc716ef6111402
dkw5877 <dkw5877@gmail.com>
2014-04-07 19:31:49 -0500

    new feature added

---
+++ GitTest/MyCustomClass.h
@@ -0,0 +1,13 @@
+//
+//  MyCustomClass.h
+//  GitTest
+//
+//  Created by user on 4/7/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyCustomClass : NSObject
+
+@end
